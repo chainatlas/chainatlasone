9a05f5b509e80efc51dcf5f2db8105f947fdbb1f
chainatlasone <solymar84@web.de>
2026-02-16 22:38:03 +0100

    Clean learn head

--- public/learn/lesson.js
+++ public/learn/lesson.js
@@ -2,9 +2,6 @@ import { LESSONS } from "./learn-data.js";
 import { LESSON_CONTENT } from "./lesson-content.js";
 import { initThemeToggle } from "./theme.js";
 import { startIllustrationLoop } from "./illustrations.js";
-requestAnimationFrame(() => {
-  document.body.classList.add("css-ready");
-});
 
 const LS_KEY = "cao_learn_progress_v1";
 
